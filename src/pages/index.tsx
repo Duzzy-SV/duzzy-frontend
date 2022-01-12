b270ff0ae55ca5e9be77fba268361032ddcf7c6c
litsynp <nocte55is@gmail.com>
2022-01-12 14:33:47 -0800

    build: initialize Next app

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>GO SV - Go Silicon Valley</title>
+        <meta name="description" content="Website for going dutch in SV" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main></main>
+    </div>
+  );
+};
+
+export default Home;
